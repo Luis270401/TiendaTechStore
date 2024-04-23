cdba43875fc26973ac2af36d69eb4ecaf0e52dce
Luis270401 <carrizozajoseluis@gmail.com>
2024-04-23 00:57:22 -0600

    j

--- script.js
+++ script.js
@@ -32,3 +32,42 @@ var swiper = new Swiper(".mySwiper-2",{
         }
     }
 });
+
+
+//Carrito
+
+const carrito = document.getElementById('carrito');
+const elementos1 = document.getElementById('lista-1'); 
+const elementos2 = document.getElementById('lista-2');
+const elementos3 = document.getElementById('lista-3');
+const lista = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
+
+cargarEventListeners();
+
+function cargarEventListeners(){
+    elementos1.addEventListener('click', comprarElemento);
+    elementos2.addEventListener('click', comprarElemento);
+    elementos3.addEventListener('click', comprarElemento);
+    carrito.addEventListener('click', eliminarElemento);
+
+    vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+
+}
+
+function comprarElemento(e){
+    e.preventDefault();
+    if(e.target.classList.contains('agregar-carrito')) {
+        const elemento = e.target.parentElement.parentElement;
+        leerDatosElemento(elemento);
+    }
+}
+
+function leeDatosElemento(elemento) {
+    const infoElemento = {
+        imagen: elemento.querySelector('img').src,
+        titulo: elemento.querySelector('h3').textContent,
+        precio: elemento.querySelector('img').src
+
+    }
+}
