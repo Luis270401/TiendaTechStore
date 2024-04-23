36385b8a52bc23ac08285f1f11b75e48a7e79984
Luis270401 <carrizozajoseluis@gmail.com>
2024-04-23 00:56:51 -0600

    p

--- script.js
+++ script.js
@@ -10,4 +10,25 @@ var swiper = new Swiper(".mySwiper-1",{
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev"
     }
-})
+});
+
+var swiper = new Swiper(".mySwiper-2",{
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    breakpoint: {
+        0: {
+            slidesPerView: 1
+        },
+        520: {
+            slidesPerView: 2
+        },
+        950: {
+            slidesPerView: 3
+        }
+    }
+});
